2ac8c1b059e90bc5d642166f1a5659b8c2551a45
Seresun <164063152+Seresun@users.noreply.github.com>
2024-08-20 19:23:19 +0000

    Deploying to gh-pages from @ Seresun/goit-js-hw-12@02606885db8e6578a83e7ed1abd56c35db33bb3d 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,10 @@
+import{S as l}from"./assets/vendor-10cb7c31.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&t(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function t(e){if(e.ep)return;e.ep=!0;const o=n(e);fetch(e.href,o)}})();async function u(s){const n=`https://pixabay.com/api/?key=45488141-0a4a6f9550d86e9b8ad8f4cbd&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`,t=await fetch(n);if(!t.ok)throw new Error("Network response was not ok");return t.json()}function f(s){const r=document.querySelector(".gallery"),n=s.map(t=>`
+    <a href="${t.largeImageURL}">
+      <img src="${t.webformatURL}" alt="${t.tags}" loading="lazy"/>
+      <div class="info">
+        <p>Likes: ${t.likes}</p>
+        <p>Views: ${t.views}</p>
+      </div>
+    </a>
+  `).join("");r.innerHTML=n}let d=new l(".gallery a");const m=document.querySelector("#search-form"),y=document.querySelector(".gallery"),c=document.querySelector(".loader");let a="";m.addEventListener("submit",p);async function p(s){if(s.preventDefault(),a=s.currentTarget.elements.searchQuery.value.trim(),!a){iziToast.error({title:"Error",message:"Please enter a search term!"});return}y.innerHTML="";try{c.style.display="block";const r=await u(a);r.hits.length===0?iziToast.warning({title:"Warning",message:"No images found!"}):(f(r.hits),d.refresh())}catch(r){iziToast.error({title:"Error",message:`Something went wrong: ${r.message}`})}finally{c.style.display="none"}}
+//# sourceMappingURL=commonHelpers.js.map
